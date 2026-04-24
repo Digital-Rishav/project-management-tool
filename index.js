f06a21971008bc056e5a7d3b00a4738271c113ac
Rishav Kumar <digitalrishavofficial@gmail.com>
2026-04-24 12:22:55 +0530

    Implemented few routes handlers

--- index.js
+++ index.js
@@ -1,64 +1,202 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const jwt = require("jsonwebtoken");
+const { authMiddleware } = require("./middleware")
 
-app.use(express.json())        //---------next 2 ----------Implenenting Routes--------->
+let USERS_ID = 1;
+let ORGANIZATION_ID = 1;
+let BOARD_ID = 1;
+let ISSUES_ID = 1;
 
-// Username, Password // USERS TABLE
-// Organisation // ORGANISATION TABLE
-// Boards // BOARDS TABLE
-// Issues // ISSUES TABLE
+const USERS = [];
 
-const USERS = [];                   //contains ---username, password, and USER_ID that will increment using global counter variable initialising at 1.
-const ORGANISATION = [];
+const ORGANIZATIONS = [];
+
 const BOARDS = [];
+
 const ISSUES = [];
 
-// CREATING ROUTES
+const app = express();
+app.use(express.json());
+
+// CREATE
 app.post("/signup", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
-})
-app.post("/signin", (req, res) => {
-
-})
-app.post("/organisation", (req, res) => {
-
-})
-app.post("/add-members-to-organisation", (req, res) => {
-
-})
-app.post("/board", (req, res) => {
-
-})
-app.post("/issue", (req, res) => {
-
-})
-
-// READING 
-app.get("/boards", (req, res) => {
-    res.send({
-        "message": "hii"
+    const userExists = USERS.find(u => u.username === username);
+    if (userExists) {
+        res.status(411).json({
+            message: "User with this username already exists"
+        })
+        return;
     }
 
-    )
+    USERS.push({
+        username,
+        password,
+        id: USERS_ID++
+    })
+
+    res.json({
+        message: "You have signed up successfully"
+    })
+
+})
+
+app.post("/signin", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const userExists = USERS.find(u => u.username === username && u.password === password);
+    if (!userExists) {
+        res.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
+
+    const token = jwt.sign({
+        userId: userExists.id
+    }, "Rishav1471password");
+    // create a jwt for the user
+
+    res.json({
+        token
+    })
+})
+
+// AUTHENTICATED ROUTE - MIDDLEWARE
+app.post("/organization", authMiddleware, (req, res) => {
+    const userId = req.userId;
+    ORGANIZATIONS.push({
+        id: ORGANIZATION_ID++,
+        title: req.body.title,
+        description: req.body.description,
+        admin: userId,
+        members: []
+    })
+
+    res.json({
+        message: "Org created",
+        id: ORGANIZATION_ID - 1
+    })
+})
+
+app.post("/add-member-to-organization", authMiddleware, (req, res) => {
+    const userId = req.userId;
+    const organizationId = req.body.organizationId;
+    const memerUserUsername = req.body.memerUserUsername;
+
+    const organization = ORGANIZATIONS.find(org => org.id === organizationId);
+
+    if (!organization || organization.admin !== userId) {
+        res.status(411).json({
+            message: "Either this org doesnt exist or you are not an admin of this org"
+        })
+        return
+    }
+
+    const memberUser = USERS.find(u => u.username === memerUserUsername);
+
+    if (!memberUser) {
+        res.status(411).json({
+            message: "No user with this username exists in our db"
+        })
+        return
+    }
+
+    organization.members.push(memberUser.id);
+
+    res.json({
+        message: "New member added!"
+    })
+})
+
+app.post("/board", (req, res) => {
     
 })
+
+app.post("/issue", (req, res) => {
+    
+})
+
+//GET endpoints
+app.get("/organization", authMiddleware, (req, res) => {
+    const userId = req.userId;
+    const organizationId = parseInt(req.query.organizationId); // "1"
+
+    const organization = ORGANIZATIONS.find(org => org.id === organizationId);
+
+    console.log(organization);
+    console.log(userId);
+    if (!organization || organization.admin !== userId) {
+        res.status(411).json({
+            message: "Either this org doesn't exist or you are not an admin of this org"
+        })
+        return
+    }
+
+    res.json({
+        organization: {
+            ...organization,
+            members: organization.members.map(memberId => {
+                const user = USERS.find(user => user.id === memberId);
+                return {
+                    id: user.id,
+                    username: user.username
+                }
+            })
+        }
+    })
+})
+
+app.get("/boards", (req, res) => {
+
+    
+})
+
 app.get("/issues", (req, res) => {
     
 })
+
 app.get("/members", (req, res) => {
-    
-})
-
-// UPDATE ROUTE--------- using path params(remember)
-app.put("/issue", (req, res) => {
-
-})
-
-//DELETE ROUTE
-app.delete("/members", (req, res) => {
 
 })
 
 
-app.listen(port)
+// UPDATE
+app.put("/issues", (req, res) => {
+
+})
+
+//DELETE -- FIND THE GBUG and fix it
+app.delete("/members", authMiddleware, (req, res) => {
+    const userId = req.userId;
+    const organizationId = req.body.organizationId;
+    const memerUserUsername = req.body.memberUserUsername;
+
+    const organization = ORGANIZATIONS.find(org => org.id === organizationId);
+
+    if (!organization || organization.admin !== userId) {
+        res.status(411).json({
+            message: "Either this org doesnt exist or you are not an admin of this org"
+        })
+        return
+    }
+
+    const memberUser = USERS.find(u => u.username === memerUserUsername);
+
+    if (!memberUser) {
+        res.status(411).json({
+            message: "No user with this username exists in our db"
+        })
+        return
+    }
+
+    organization.members = organization.members.filter(user => user.id !== memberUser.id);
+
+    res.json({
+        message: "member deleted!"
+    })
+})
+
+app.listen(3000);
